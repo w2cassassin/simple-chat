a08ba9912377ee162dc1aa75c3b1bf6923a074c1
Подколзин Кирилл <pokir03@gmail.com>
2024-12-20 18:51:35 +0300

    Fixes

--- backend/main.py
+++ backend/main.py
@@ -3,20 +3,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from app import models
 from app.routes import router
+import os
+from dotenv import load_dotenv
 
-app = FastAPI()
+load_dotenv()
+
+app = FastAPI(root_path=os.getenv('ROOT_PATH', '/chat/api'))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(models.Base.metadata.create_all)
+
 
 app.include_router(router)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename="/chat">
       <div className="App">
         <Switch>
           <Route exact path="/">
